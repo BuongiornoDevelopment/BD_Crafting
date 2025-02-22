b2685b2865dc2a314c07b4721223047285927642
Samuele <samusonoio24@gmail.com>
2025-02-22 13:09:45 +0100

    Update server.lua

--- Server/server.lua
+++ Server/server.lua
@@ -7,14 +7,12 @@ AddEventHandler('bd_crafting:iniziaCrafting', function(craftingId, ricettaId)
     local ricetta = crafting.ricette[ricettaId]
     
     if crafting.job and xPlayer.job.name ~= crafting.job then
-        TriggerClientEvent('bd_crafting:notifica', source, 'Non hai il lavoro richiesto')
         return
     end
     
     for _, ingrediente in pairs(ricetta.ingredienti) do
         local item = exports.ox_inventory:GetItem(source, ingrediente.item)
         if not item or item.count < ingrediente.quantita then
-            TriggerClientEvent('bd_crafting:notifica', source, 'Non hai abbastanza materiali')
             return
         end
     end
@@ -24,5 +22,4 @@ AddEventHandler('bd_crafting:iniziaCrafting', function(craftingId, ricettaId)
     end
     
     exports.ox_inventory:AddItem(source, ricetta.risultato.item, ricetta.risultato.quantita)
-    TriggerClientEvent('bd_crafting:notifica', source, 'Crafting completato con successo')
 end)
